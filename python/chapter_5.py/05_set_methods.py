015ceef7dda5523d107f04a760196981411cf246
0mAnand <omanand.piro@gmail.com>
2023-03-15 17:37:39 +0530

    f

--- python/chapter_5.py/05_set_methods.py
+++ python/chapter_5.py/05_set_methods.py
@@ -1,30 +1,30 @@
-# creating an empty set 
-b = set()               
-print(type(b))
+# # creating an empty set 
+# b = set()               
+# print(type(b))
 
-# adding values to an empty set 
-b.add(4)                 
-b.add(4)             # set is a collection of non repeting element
-b.add(5)
-b.add(5)
-#b.add({4,5}) # can not list or dictionary to set
-b.add((4,6,8)) # we can add tuple in a set 
-print(b)
+# # adding values to an empty set 
+# b.add(4)                 
+# b.add(4)             # set is a collection of non repeting element
+# b.add(5)
+# b.add(5)
+# #b.add({4,5}) # can not list or dictionary to set
+# b.add((4,6,8)) # we can add tuple in a set 
+# print(b)
 
-print(len(b))   # prints the length of this set
+# print(len(b))   # prints the length of this set
 
-b.remove(5) # remove 5 from the set b
-# b.remove(15) # throws an error while trying to remove 15 (which is not present in the set)
-print(b)
+# b.remove(5) # remove 5 from the set b
+# # b.remove(15) # throws an error while trying to remove 15 (which is not present in the set)
+# print(b)
 
-print(b.pop())
-print(b)
+# print(b.pop())
+# print(b)
 
-b.clear()
-print(b)
+# b.clear()
+# print(b)
 
-b.union( )
-print(b)
+# b.union()
+# print(b)
  
 
 

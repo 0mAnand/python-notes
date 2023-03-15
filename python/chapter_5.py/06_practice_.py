6839fdf2badb56fd33f37ee3ea0a96743de9f746
0mAnand <omanand.piro@gmail.com>
2023-03-15 14:39:07 +0530

    Update 06_practice_.py

--- python/chapter_5.py/06_practice_.py
+++ python/chapter_5.py/06_practice_.py
@@ -1,1 +1,61 @@
-# 05_practice_01
+# # 05_practice_01
+# mydict = {
+#     "pankha": "fan",
+#     "dabba": "box",
+#     "vastu": "items"
+# }
+# print("optins are",mydict.keys())
+# a = input("enter the hindi words\n")
+# print("the meaning of our world is ",mydict[a])
+
+# #below line will not throws an error if the key is not present in the dictionary
+# # print("the meaning of our world is ",mydict.get(a))
+
+# # 05_practice_02
+# num1 = int(input("enter number 1\n"))
+# num2 = int(input("enter number 2\n"))
+# num3 = int(input("enter number 3\n"))
+# num4 = int(input("enter number 4\n"))
+# num5 = int(input("enter number 5\n"))
+# num6 = int(input("enter number 6\n"))
+# num7 = int(input("enter number 7\n"))
+# num8 = int(input("enter number 8\n"))
+
+# s = {num1, num2, num3, num4, num5, num6, num7, num8}
+# print(s)
+
+# # 05_practice_03
+# s = {18 , "18", 18.1}
+
+# print(s)
+# print(type(s))
+# print(len(s))
+
+# # 05_practice_04
+# s = {20, 20.0, "20"}
+# print(s)
+# print(len(s))
+
+# 05_practice_05
+
+# #s = {}     # empty dictionary
+# s = set()    # empty set
+# print(type(s))
+
+# # 05_practice_06
+
+# favlang = {}
+# a = input("Enter your favorite language pratham\n")
+# b = input("Enter your favorite language ankit\n")
+# c = input("Enter your favorite language sonali\n")
+# d = input("Enter your favorite language harshita\n")
+
+# favlang['pratham'] = a 
+# favlang['ankit'] = b
+# favlang['sonali'] = c
+# favlang['harshita'] = d
+# # favlang['pratham'] = c        # 05_practice_07
+# print(favlang)
+
+# 05_practice_09
+# no  sets cannot contains list  ,  que is wrong

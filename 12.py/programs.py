06dc76f78cc6b1e1795dedd8cfd5de0d25bd531e
0mAnand <omanand.piro@gmail.com>
2023-06-19 22:12:28 +0530

    .
    .

---
+++ 12.py/programs.py
@@ -0,0 +1,70 @@
+#Q1
+# ans = input("are you ready to provide your card number(1=Yes/0=No):")
+# print("your chouse:", ans )
+# if(ans=="1"):
+#     print("Now we will continue the processing of transaction")
+# elif(ans=="0"):
+#     print("Now we will not contiune the processing the transaction")
+
+#Q2
+# n = int(input("Enter your number:"))
+# print("The number is:",n)
+# d = n%2
+# if (d==0):
+#     print("Remainder on division by 2 is:",d)
+#     print("The number is an even number.")
+# elif(d!=0):
+#     print("Remainder on division by 2 is:",d)
+#     print("The number is an odd number.")
+# # this program is also written as 
+# n = int(input("Enter your number:"))
+# print("The number is:",n)
+# d = n%2
+# if (d==0):
+#     print("Remainder on division by 2 is:",d)
+#     print("The number is an even number.")
+# else:
+#     print("Remainder on division by 2 is:",d)
+#     print("The number is an odd number.")
+
+#Q3
+# ch = input("Enter a character:")
+# if(((ch>='A')and (ch<='z')) or ((ch>='a') and (ch<='z'))):
+#     print("you entered a alphabet")
+# elif(ch>='0' and ch<='9'):
+#     print("You entered a digit")
+# else:
+#     print("you enter a character other then alphabet and digits")
+
+#Q4
+# n=int(input("Enter the limit:"))
+# i = 1
+# a = 0
+# b = 1
+# while(i<=n):
+#     print(a)
+#     c = a+b
+#     a = b
+#     b = c
+#     i = i+1
+
+#Q5
+# num = int(input("Enter a number:"))
+# n = num
+# f = 1
+# while(n>0):
+#     f = f*n
+#     n = n-1
+# print("the factorial of", num , "is", f)
+
+#Q6
+l = int(input("Enter the length of the cuboid: "))
+b = int(input("Enter the breadth of the cuboid: "))
+h = int(input("Enter the Height of the cuboid: "))
+volume = l * b * h
+print= ("length=",l)
+print= ("breadth=",b)
+print = ("height=",h)
+print("The volume of cuboid=",volume)
+    
+#Q7
--- python/chapter_6.py/06_practice_.py
+++ python/chapter_6.py/06_practice_.py
@@ -33,19 +33,19 @@
 # else:
 #     print("you are pass")
 
-# 06_practice_03
-text = input("Enter the text")
-spam = False
+# # 06_practice_03
+# text = input("Enter the text")
+# spam = False
 
-if("make a lot of money" in text):
-    spam = True
-elif("buy now" in text):
-    spam =True
-elif("click this" in text):
-    spam = True
-elif("subscribe this" in text):
-    spam = True
-else:
-    spam = False
+# if("make a lot of money" in text):
+#     spam = True
+# elif("buy now" in text):
+#     spam =True
+# elif("click this" in text):
+#     spam = True
+# elif("subscribe this" in text):
+#     spam = True
+# else:
+#     spam = False
 
 
